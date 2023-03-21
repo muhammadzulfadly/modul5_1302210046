9bf3c6cef513bb8751e026c2438a6c042580b3b6
PRAKTIKAN <muhammadzulfadly@student.telkomuniversity.ac.id>
2023-03-21 16:48:27 +0700

    menambahkan class SimpleDataBase

--- Program.cs
+++ Program.cs
@@ -8,8 +8,43 @@ namespace modul5_1302210046
 {
     internal class Program
     {
+        public class SimpleDataBase<T>
+        {
+            private List<T> storedData;
+            private List<DateTime> inputDates;
+
+            public SimpleDataBase() 
+            {
+                this.storedData = new List<T>();
+                this.inputDates = new List<DateTime>();
+            }
+
+            public void AddNewData(T data)
+            {
+            
+                this.storedData.Add(data);
+                this.inputDates.Add(DateTime.Now);
+   
+            }
+
+            public void PrintAllData()
+            {
+                for(int i = 0; i < storedData.Count(); i++)
+                {
+                    Console.WriteLine("Data " + i + " berisi " + storedData[i] + 
+                        ", yang disimpan pada waktu UTC: " + inputDates[i]);
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
+            SimpleDataBase<int> data = new SimpleDataBase<int>();
+            data.AddNewData(13);
+            data.AddNewData(02);
+            data.AddNewData(21);
+            data.PrintAllData();
+
         }
     }
 }
